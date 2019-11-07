53408ee6be5d7f77edf4ed62443675c92d4757c3
Vale-S <valentin.skok@gmail.com>
2019-11-07 17:53:28 +0100

    Dodal kontaktno formo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #import knjiznice 2 nacina
-from flask import Flask, render_template
+#request lahko uporabljamo samo za POST methodo
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,11 +13,21 @@ def kontakt ():
     emaili = ["super.svizec@gmail.com", "avto.svizec@email.com", "cars.svizec@email.com"]
     return render_template ("kontakt.html", emaili=emaili)
 
+#dodaj metodo....
+@app.route("/poslji-sporocilo", methods=["POST"])
+def poslji_sporocilo():
+    zadeva = request.form.get("zadeva")
+    sporocilo = request.form.get("sporocilo")
+
+    #tukaj bi shranili te spremenljivki v bazo
+
+    print ("Zadeva je: " + zadeva)
+    print ("Sporocilo je: " + sporocilo)
+    return "Hvala za poslano zadevo: " + zadeva + " " + sporocilo
+
 @app.route("/omeni")
 def omeni ():
     return render_template ("omeni.html")
-
-
 
 if __name__ == '__main__':
     app.run()
