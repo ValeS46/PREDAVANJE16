8d0e17595484fc9391928b94999932f8ebc5858d
Vale-S <valentin.skok@gmail.com>
2019-11-25 18:10:56 +0100

    Dodal prikaz uporabnika

--- main.py
+++ main.py
@@ -167,5 +167,11 @@ def uporabnik():
     users = db.query(Uporabnik).all()
     return render_template("uporabniki.html", uporabniki=users)
 
+#db.querry izpiši!!
+@app.route("/uporabniki/<uporabnik_id>")
+def prikaz_uporabnika(uporabnik_id):
+    uporabnik = db.query(Uporabnik).filter_by(id=uporabnik_id).first()
+    return render_template("prikaz_uporabnika.html", uporabnik=uporabnik)
+
 if __name__ == '__main__':
     app.run(debug=True)
